1d6a70b818df0070c6765020f2551a286cca0592
李旺理 <wangli.li@stringstech.co>
2020-01-07 09:53:50 +0800

    wepy项目

---
+++ weapp/pages/user/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _core = _interopRequireDefault(require('./../../vendor.js')(0));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_core["default"].page({
+  data: {},
+  methods: {},
+  config: {
+    con: con
+  }
+}, {info: {"components":{},"on":{}}, handlers: {}, models: {}, refs: undefined });
